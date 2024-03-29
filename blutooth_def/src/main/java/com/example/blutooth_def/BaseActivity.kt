7a9d8c6ad5076ec66589d232e197f26cf297a7b8
mosen <cool.snow@mail.ru>
2024-03-29 22:52:08 +0300

    Fragment list

--- blutooth_def/src/main/java/com/example/blutooth_def/BaseActivity.kt
+++ blutooth_def/src/main/java/com/example/blutooth_def/BaseActivity.kt
@@ -20,22 +20,7 @@ class BaseActivity : AppCompatActivity() {
         binding = ActivityBaseBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_base)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_base)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
