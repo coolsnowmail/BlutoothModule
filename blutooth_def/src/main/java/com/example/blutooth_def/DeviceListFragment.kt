89b145dabe8432d9864168b55b8762774a937f40
mosen <cool.snow@mail.ru>
2024-04-05 10:12:50 +0300

    resycleView 6 done but not work

--- blutooth_def/src/main/java/com/example/blutooth_def/DeviceListFragment.kt
+++ blutooth_def/src/main/java/com/example/blutooth_def/DeviceListFragment.kt
@@ -1,22 +1,29 @@
 package com.example.blutooth_def
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.R
 import androidx.core.graphics.drawable.DrawableCompat
 import com.example.blutooth_def.databinding.FragmentListBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class DeviceListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
     private var bthAdapter: BluetoothAdapter? = null
+    private lateinit var bthLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +35,10 @@ class DeviceListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imageBTNOn.setOnClickListener {
+            bthLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+        }
+        registerBthLauncher()
         initAdapter()
         bluetoothState()
     }
@@ -41,5 +52,19 @@ class DeviceListFragment : Fragment() {
     private fun initAdapter() {
         val bthManager = activity?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bthAdapter = bthManager.adapter
+    }
+
+    private fun registerBthLauncher() {
+        bthLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if(it.resultCode == Activity.RESULT_OK) {
+                changeButtonColor(binding.imageBTNOn, Color.GREEN)
+                Snackbar.make(binding.root, "Блютуз включен", Snackbar.LENGTH_LONG).show()
+            } else {
+                changeButtonColor(binding.imageBTNOn, Color.RED)
+                Snackbar.make(binding.root, "Блютуз отключен", Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
